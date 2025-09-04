b37fa65a9e41cce05547e8ce1347dec3c635060a
Yashveerdass <yram0011@student.monash.edu>
2025-09-04 13:19:05 +0800

    (3)Uusers(db): create users table with core fields
    - Added users table with fields:
      user_id, email, password_hash, role, created_at
    
    - Enforced unique constraint on email
    
    - Set default timestamp for created_at

--- Backend/DB/init.sql
+++ Backend/DB/init.sql
@@ -8,3 +8,14 @@ DROP TABLE IF EXISTS course CASCADE;
 DROP TABLE IF EXISTS instructor_profile CASCADE;
 DROP TABLE IF EXISTS student_profile CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
+
+-- USERS
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+
